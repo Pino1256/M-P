8bb2f01161d7f33ff8cfa88c5dd34c35d419d0ea
Pietro Rupil <rupilpietro@gmail.com>
2026-04-22 10:45:53 +0200

    file che apre al finestra di gioco

---
+++ gioco.py
@@ -0,0 +1,18 @@
+import arcade
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+
+class GameWindow(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "UPP")
+        
+        menu = 
+        self.show_view(menu)
+
+def main():
+    window = GameWindow()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
