7b5a53b6775ee504632dd3c1b72c25de2c230209
Pietro Rupil <rupilpietro@gmail.com>
2026-04-22 11:24:03 +0200

    modifiche

--- player.py
+++ player.py
@@ -4,3 +4,7 @@ from sprite_animato import SpriteAnimato
 class Player(SpriteAnimato): 
     def __init__(self):
         super().__init__(scale = 1)
+
+        self.file_animazioni = {
+            "diestra": "assets/"
+        }
